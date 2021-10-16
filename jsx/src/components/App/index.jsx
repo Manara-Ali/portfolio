c5dc65a9434a88339bbfb066de2890e46dab2c28
Manara Ali <manaraali64@gmail.com>
2021-10-16 09:45:50 -0400

    Delete initial boilerplate and create new set up for my application

---
+++ jsx/src/components/App/index.jsx
@@ -0,0 +1,21 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. ADDITIONAL IMPORTS HERE
+
+// 3. AUTO RELOAD
+if (module.hot) {
+  module.hot.accept();
+}
+
+// 4. CREATE A FUNCTION BASED COMPONENT
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+// 5. EXPORT COMPONENT TO BE USED IN OTHER PARTS OF OUR APPLICATION
+export default App;
---
+++ jsx/src/index.jsx
@@ -0,0 +1,11 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. IMPORT REACTDOM
+import ReactDOM, { render } from "react-dom";
+
+// 3. ADDITIONAL IMPORTS GO HERE
+import App from "./components/App";
+
+// 4. RENDER APPLICATION TO THE SCREEN
+ReactDOM.render(<App />, document.querySelector("#root"));
